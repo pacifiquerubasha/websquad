ac608709635b74d3a65e23d7c144b07fd9d92ef0
Pacifique Rubasha <Pacifiquer@frci.local>
2023-06-02 12:46:57 +0400

    Fixed Header Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { menuItems } from '../utils/content';
 
 function Header(props) {
+
+    const [isFixed, setIsFixed] = useState();
+
+    useEffect(() => {
+        const handleScroll = () => {
+          setIsFixed(window.scrollY > window.innerHeight)
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+
     return (
-        <header className='flex'>
+        <header className={`flex ${isFixed && "fixedHeader"}`}>
             <a href='/' className='logo'><span className='text-blue'>Job</span>Magnetix</a>
             <nav>
                 <ul className='flex'>
